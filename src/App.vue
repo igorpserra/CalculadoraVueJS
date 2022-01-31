ea8a08304f53b46e6572b9f862840792659bd169
igorpserra <igorpserra@gmail.com>
2022-01-31 01:09:55 -0300

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,34 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <h1>hello world!</h1>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    @font-face {
+        font-family: "RobotoMono";
+        src: url('./fonts/RobotoMono-Thin.ttf');
+    }
+
+    *{
+        font-family: "RobotoMono", monospace;
+    }
+
+    #app{
+        display: flex;
+        flex-direction: column;
+        height: 100vh;
+        justify-content: center;
+        align-items: center;
+        text-align: center;
+
+        color: white;
+        background: linear-gradient(to right, rgb(83,105,118), rgb(41,46,73));
+    }
 </style>
